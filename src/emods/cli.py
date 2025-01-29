f298c711ab68336cb453dd556457a25ebfad6fcd
amc-corey-cox <69321580+amc-corey-cox@users.noreply.github.com>
2025-01-29 10:58:01 -0600

    Cookie-cutter setup

---
+++ src/emods/cli.py
@@ -0,0 +1,44 @@
+"""Command line interface for emods."""
+import logging
+
+import click
+
+from emods import __version__
+from emods.main import demo
+
+__all__ = [
+    "main",
+]
+
+logger = logging.getLogger(__name__)
+
+
+@click.group()
+@click.option("-v", "--verbose", count=True)
+@click.option("-q", "--quiet")
+@click.version_option(__version__)
+def main(verbose: int, quiet: bool):
+    """
+    CLI for emods.
+
+    :param verbose: Verbosity while running.
+    :param quiet: Boolean to be quiet or verbose.
+    """
+    if verbose >= 2:
+        logger.setLevel(level=logging.DEBUG)
+    elif verbose == 1:
+        logger.setLevel(level=logging.INFO)
+    else:
+        logger.setLevel(level=logging.WARNING)
+    if quiet:
+        logger.setLevel(level=logging.ERROR)
+
+
+@main.command()
+def run():
+    """Run the emods's demo command."""
+    demo()
+
+
+if __name__ == "__main__":
+    main()
